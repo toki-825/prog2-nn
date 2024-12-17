d0a5e6e91a57f862ae5b6af18f77a230a3cff34c
Yamamoto <j3373fba@sit.ac.jp>
2024-12-17 15:42:13 +0900

    1エポックの時間計測

--- train.py
+++ train.py
@@ -89,10 +89,13 @@ acc_train_history = []
 acc_test_history = []
 for k in range(n_epochs):
     print(f'epoch {k+1}/{n_epochs}', end = ': ',flush=True)
-
+    
+    # 1 epoch の学習
+    time_start = time.time()
     loss_train = models.train(model, dataloader_train, loss_fn, optimizer)
+    time_end = time.time()
     loss_train_history.append(loss_train)
-    print(f'train loss: {loss_train:3f}', end=', ')
+    print(f'train loss: {loss_train:3f} ({time_end-time_start}s)', end=', ')
     
     loss_test = models.test(model, dataloader_test, loss_fn)
     loss_test_history.append(loss_test)
