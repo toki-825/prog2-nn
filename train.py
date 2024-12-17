06eff9a300bc3b70436f227b93710125b161cc13
Yamamoto <j3373fba@sit.ac.jp>
2024-12-17 15:49:52 +0900

    他の時間計測

--- train.py
+++ train.py
@@ -97,17 +97,24 @@ for k in range(n_epochs):
     loss_train_history.append(loss_train)
     print(f'train loss: {loss_train:3f} ({time_end-time_start:.1f}s)', end=', ')
     
+    time_atart = time.time()
     loss_test = models.test(model, dataloader_test, loss_fn)
+    time_end = time.time()
     loss_test_history.append(loss_test)
-    print(f'test loss: {loss_test:3f}', end=', ')
+    print(f'test loss: {loss_test:3f} ({time_end-time_start:.1f}s)', end=', ')
 
     # 精度を計算する
+    time_start = time.time()
     acc_train = models.test_accuracy(model, dataloader_test)
+    time_end = time.time()
     acc_train_history.append(acc_train)
-    print(f'test accuracy: {acc_test*100:.3f}%', end=', ')
+    print(f'test accuracy: {acc_test*100:.3f}% ({time_end-time_start:.1f}s)', end=', ')
+    
+    time_start = time.time()
     acc_test = models.test_accuracy(model,dataloader_test)
+    time_end = time.time()
     acc_test_history.append(acc_test)
-    print(f'test accuracy: {acc_test*100:.3f}%', end=', ')
+    print(f'test accuracy: {acc_test*100:.3f}% ({time_end-time_start:.1f}s)', end=', ')
 
 
 plt.plot(acc_train_history, labrl = 'train')
