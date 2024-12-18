275a3ecfc4e1032d0f9d38fba6899e4bf9bc4cf0
Yamamoto <j3373fba@sit.ac.jp>
2024-12-18 21:58:32 +0900

    trainに追加

--- train.py
+++ train.py
@@ -9,6 +9,9 @@ import torchvision.transforms.v2 as transforms
 
 import models
 
+# GPUがあれば 'cuda' なければ 'cpu' というデバイス名を設定
+device = 'cuda' if torch.cuda.is_availabel() else 'cpu'
+
 
 # データセットの前処理を定義
 ds_transform = transforms.Compose([
@@ -53,7 +56,9 @@ for image_batch, label_batch in dataloader_test:
 model = models.MyModel()
 
 # 精度を計算する
-acc_test = models.test_accuracy(model, dataloader_test)
+acc_train = models.test_accuracy(model, dataloader_train, device=device)
+print(f'train accuracy: {acc_train*100:.3f}%')
+acc_test = models.test_accuracy(model, dataloader_test, device = device)
 print(f'test accuary: {acc_test*100:.3f}%')
 
 # 損失関数（誤差関数・ロス関数）の選択
@@ -93,20 +98,20 @@ for k in range(n_epochs):
     if (k+1) % 5 ==0:
     # 1 epoch の学習
       time_start = time.time()
-      loss_train = models.train(model, dataloader_train, loss_fn, optimizer)
+      loss_train = models.train(model, dataloader_train, loss_fn, optimizer, device=device)
       time_end = time.time()
       loss_train_history.append(loss_train)
       print(f'train loss: {loss_train:3f} ({time_end-time_start:.1f}s)', end=', ')
      
       time_atart = time.time()
-      loss_test = models.test(model, dataloader_test, loss_fn)
+      loss_test = models.test(model, dataloader_test, loss_fn, device=device)
       time_end = time.time()
       loss_test_history.append(loss_test)
       print(f'test loss: {loss_test:3f} ({time_end-time_start:.1f}s)', end=', ')
 
     # 精度を計算する
       time_start = time.time()
-      acc_train = models.test_accuracy(model, dataloader_test)
+      acc_train = models.test_accuracy(model, dataloader_test, device = device)
       time_end = time.time()
       acc_train_history.append(acc_train)
       print(f'test accuracy: {acc_test*100:.3f}% ({time_end-time_start:.1f}s)', end=', ')
